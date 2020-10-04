39f5b655f68f555a27368fb19f88c0e172d4d704
valerpenko <vpenko@onu.edu.ua>
2020-10-04 09:46:42 +0300

    horizon v2.py - две функции исправлена ошибка: R->h

--- function/horizon v2.py
+++ function/horizon v2.py
@@ -1,84 +1,15 @@
 def horizonlen(R,h):
-    H=(R+h)**2-(h**2)
+    H=(R+h)**2-(R**2)
     H=H**0.5
     return H
 
-
-
-def HorCalc(m, h, bh, r, planet ):
-    man = horizonlen(r,m)
-    house = horizonlen(r,h)
-    BH = horizonlen(r,bh)
+def HorCalc(r, planet ):
+    man = horizonlen(r,0.002)
+    house = horizonlen(r,0.03)
+    BH = horizonlen(r,0.828)
     print(man, house, BH,"-",planet)
 
 
-HorCalc(0.002,0.03,0.828,6371, "Earth")
-HorCalc(0.002,0.03,0.828,3389,"mars")
+HorCalc(6371, "Earth")
+HorCalc(3389,"mars")
 
-
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Венера")
-even(0.002,0.03,0.828,6051)
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Юпитер")
-even(0.002,0.03,0.828,69911)
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Меркурий")
-even(0.002,0.03,0.828,2439)
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Сатурн")
-even(0.002,0.03,0.828,58232)
-
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Нептун")
-even(0.002,0.03,0.828,24622)
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Уран")
-even(0.002,0.03,0.828,25362)
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Плутон")
-even(0.002,0.03,0.828,1188)
-
-print("Дополнительно: ")
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Луна")
-even(0.002,0.03,0.828,1737)
-
-# def even(m, h, bh, planet):
-#     man = ((((m + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     house = ((((h + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     BH = ((((bh + planet) ** 2) - (planet ** 2)) ** 0.5)
-#     print(man, house, BH,"-Солнце")
-even(0.002,0.03,0.828,696340)
